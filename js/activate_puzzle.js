d25f5a0decbf593e47ca7e30b0af8c0c22626ed9
Pavan-1311 <103481768+Pavan-1311@users.noreply.github.com>
2023-05-03 05:08:50 +0530

    Add files via upload

---
+++ js/activate_puzzle.js
@@ -0,0 +1,58 @@
+var options = {
+				   solution: 			[[null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+		                                [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,    'K',   null,   null,   null,   null,   null,   null,   null],
+		                                [null,	null,	null,	null,	null,	null,	null,	null,	null,    'N',    'O',    'B',    'I',    'T',    'H',    'A',   null,   null,   null,   null],
+		                                [null,	null,	null,	 'A',	 'B',	 'S',	 'E',	 'N',	'T',  	null,   null,   null,    'D',   null,   null,    null,   null,   null,   null,   null],
+		                                [null,	null,	null,	null,	null,	 'H',	null,	null,	null, 	null,   null,    'E',    'N',    'Q',    'U',    'I',    'R',    'Y',   null,   null],
+		                                [null,	null,	null,	null,	null,	 'E',	null,	null,	null,   null,   null,   null,    'A',   null,   null,    null,   null,   null,   null,   null],
+		                                [null,	null,	null,	null,	null,	 'L',	 'H',	null,	null,    'D',   null,   null,    'P',   null,   null,    null,   null,   null,   null,   null],
+		                                [null,	null,	null,	null,	 'S',	 'L',	 'I',	 'P',	 'P',    'E',    'R',   null,   null,   null,   null,    null,   null,   null,   null,   null],
+		                                [null,	null,	null,	null,	null,	 null,	 'S',	null,	null,    'A',   null,   null,   null,   null,   null,    null,   null,   null,   null,   null],
+		                                [null,	null,	null,	null,	null,	 null,	 'T',	null,	null,    'T',   null,   null,   null,   null,   null,    null,   null,   null,   null,   null],
+		                                [null,	null,	null,	null,	null,	 null,	 'O',	null,	null,    'H',   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+		                                [null,	null,	null,	null,	null,	 null,	 'R',	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+		                                [null,	null,	null,	null,	null,	 null,	 'Y',	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+		                                [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+		                                [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+		                                [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+		                                [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+		                                [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+		                                [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+		                                [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null]],
+
+
+		            index: 				[[null,  null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+		                                [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,      4,   null,   null,   null,   null,   null,   null,   null],
+		                                [null,   null,   null,   null,   null,   null,   null,   null,   null,      6,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+		                                [null,   null,   null,      1,   null,      2,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+		                                [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,      5,   null,   null,   null,   null,   null,   null,   null,   null],
+		                                [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+		                                [null,   null,   null,   null,   null,   null,      7,   null,   null,      8,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+		                                [null,   null,   null,   null,      3,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+		                                [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+		                                [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+		                                [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+		                                [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+		                                [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+		                                [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+		                                [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+		                                [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+		                                [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+		                                [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+		                                [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+		                                [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null]],
+
+		         questions_across: [	  "1. Missing",
+		                                  "3. Provide comfort and protection for feet",
+		                                  "5. Investigation",
+		                                  "6. Doraemon's friend"],
+
+		         questions_down: [		 "2. Shirt",
+		                                 "4. Capture",
+		                                 "7. In the past",
+		                                 "8. Opposite word of Life"]
+
+	};
+
+
+		cwp_nmsp.activateCWP(document.getElementById("myPuzzle"),false,options);
