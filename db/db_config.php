5c212ac91d351118f18a10bda17b342f83929590
Pavan-1311 <103481768+Pavan-1311@users.noreply.github.com>
2023-05-03 04:58:26 +0530

    Create db_config.php

---
+++ db/db_config.php
@@ -0,0 +1,15 @@
+<?php
+// database configuration
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$database = 'sample';
+
+// establish database connection
+$conn = mysqli_connect($host, $user, $password, $database);
+
+// check if connection was successful
+if(!$conn) {
+	die('Database connection error: ' . mysqli_connect_error());
+}
+?>
